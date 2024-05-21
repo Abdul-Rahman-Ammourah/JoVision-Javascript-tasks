810b9f21bba7459e70c807471df1fdb9dcacb8c3
Abdul-Rahman Ammourah <abdulrhmanammourah@gmail.com>
2024-05-21 10:33:42 +0300

    brief changes and a new task

--- Task#12/Index.js
+++ Task#12/Index.js
@@ -1,7 +1,7 @@
 function Submitdata(){
    const user = {Name : document.getElementById("Name").value,
                     Age : document.getElementById("Age").value,
-                    Time: new Date().toLocaleTimeString};
+                    Time: new Date()};
 
         window.alert("Your Name is: " + user.Name + "\n" + "Your Age is: " + user.Age + "\n" + "Submitted time: " + user.Time);
 
